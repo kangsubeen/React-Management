e8b7f72bec4e35b710ca610034932d2057425a0e
kang subeen <42593251+kangsubeen@users.noreply.github.com>
2019-07-13 04:51:25 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,24 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-    'id': 1,
-    'image': 'https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201904%2F20190418150724431.jpg',
-    'name': '황민현',
-    'birthday': '950809',
-    'height': '181cm',
-    'job': "NU'EST"
-  },
-  {
-    'id': 2,
-    'image': 'https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201901%2F20190117122557308.jpg',
-    'name': '옹성우',
-    'birthday': '950824',
-    'height': '179cm',
-    'job': 'Actor'
-  },
-  {
-    'id': 3,
-    'image': '2w3https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2F22%2F201712261720546481.jpg',
-    'name': '이한결',
-    'birthday': '991207',
-    'height': '177cm',
-    'job': 'IM'
-  }
-]
-
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -65,12 +55,10 @@ class App extends Component {
           </TableHead>
           <TableBody>
             {
-              customers.map(c => {
-                return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} height={c.height} job={c.job}
-                />
+              this.state.customers ? this.state.customers.map(c => {
+                return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} height={c.height} job={c.job} />
                 )
-              })
-            }
+              }) : "" }
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({ message: 'Hello Express !' });
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+          'id': 1,
+          'image': 'https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201904%2F20190418150724431.jpg',
+          'name': '황민현',
+          'birthday': '950809',
+          'height': '181cm',
+          'job': "NU'EST"
+        },
+        {
+          'id': 2,
+          'image': 'https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201901%2F20190117122557308.jpg',
+          'name': '옹성우',
+          'birthday': '950824',
+          'height': '179cm',
+          'job': 'Actor'
+        },
+        {
+          'id': 3,
+          'image': '2w3https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2F22%2F201712261720546481.jpg',
+          'name': '이한결',
+          'birthday': '991207',
+          'height': '177cm',
+          'job': 'IM'
+        }
+      ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
